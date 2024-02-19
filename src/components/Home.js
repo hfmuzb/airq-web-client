6bf7a1282e2921f19e8099f392f5d90de2d9a232
Khusniddin Fakhritdinov <kh.fakhritdinov@gmail.com>
2024-02-19 16:56:55 +0500

    feat: landing page

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="description">
+        <h1>Welcome to Your Single Page Web App!</h1>
+        <p>Add your description here...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
